16fb2a902757dcfee177215060f7bacaea979c05
Cowarky <malik1sabehayoun@gmail.com>
2023-05-14 19:21:35 +0300

    preparing to add products

--- clothUp/resources/views/layout/app.blade.php
+++ clothUp/resources/views/layout/app.blade.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
 
     <title>Clothup - buy modern clothes here</title>
 </head>
+
 <body>
     <nav class="navbar-container">
         <div class="navbar">
             <div class="logo-container">
-                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qeJOG5X6Q6P5OqOqgxWpiAHaEq%26pid%3DApi&f=1&ipt=a38af0f4ceb5fa7cf5be97f1609af44406b89815151e0d5e1c949ed829c43a5d&ipo=images" alt="">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qeJOG5X6Q6P5OqOqgxWpiAHaEq%26pid%3DApi&f=1&ipt=a38af0f4ceb5fa7cf5be97f1609af44406b89815151e0d5e1c949ed829c43a5d&ipo=images"
+                    alt="">
             </div>
             <ul>
                 <li>
-                    <a href="">
+                    <a href="/">
                         Home
                     </a>
                 </li>
@@ -37,10 +41,10 @@
                 </li>
             </ul>
         </div>
-        
+
     </nav>
-    <div class="products">
-        Products
-    </div>
+
+
 </body>
+
 </html>
